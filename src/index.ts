49d74ae8292af4fc0de015ac0b330cd4e2f6b0d5
vtranwd <vtranwd@gmail.com>
2026-08-25 16:07:14 -0700

    made CORS less strict

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ async function main() {
 
   // Add CORS middleware
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
