4853b4e2cb6f28b508b62e9904e8d9011cad2e53
vtranwd <vtranwd@gmail.com>
2026-09-17 17:00:37 -0700

    TypeScript type case

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,10 @@ async function main() {
     const allowedOrigins = [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://feedback-loop-frontend-xyz.vercel.app', // Replace with your actual Vercel URL
+      'https://feedback-loop-frontend.vercel.app/', // Replace with your actual Vercel URL
     ];
     
-    const origin = req.headers.origin;
+    const origin = req.headers.origin as string;
     if (allowedOrigins.includes(origin)) {
       res.header('Access-Control-Allow-Origin', origin);
     } else {
